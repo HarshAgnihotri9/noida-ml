14243615b18876a1e2416f57611932f0da60edbf
Harsh <agnihotriharsh154@gmail.com>
2026-01-14 07:16:18 -0800

    test cases changes

--- src/test.py
+++ src/test.py
@@ -3,3 +3,23 @@ import sklearn
 
 print("Pandas:", pd.__version__)
 print("Sklearn:", sklearn.__version__)
+
+# creating dataframe
+
+# mydataset=({
+#     'id':[1,2,3,4,5,6,7,8],
+#     'studentName':["harsh","hari","harsita","rmoabdi","jdwhbhjw","devdjb","djhkss","harsihsj"]
+# })
+
+# df1=pd.DataFrame(mydataset)
+
+# print(df1)
+# print(pd.__version__)
+  
+
+
+# SERIES
+
+# a=[1,3,5]
+# myvar = pd.Series(a)
+# print(myvar)
