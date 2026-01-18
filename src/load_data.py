f08448481a43585625c931f3567b2bc8b3a3a5e8
Harsh <agnihotriharsh154@gmail.com>
2026-01-18 10:04:08 -0800

    somee ai learning

--- src/load_data.py
+++ src/load_data.py
@@ -5,7 +5,17 @@ df = pd.read_csv("data/properties.csv")
 
 # print(df.info())
 # print(df.describe())
-print(df.head(10))
+# print(df.head(10))
+# print(df.head())
+# print(df.tail())
+
+# print(df.shape)
+
+print(df.columns)
+
+print(df.info())
+
+print(df[["sector", "society", "rate", "area"]])
 # # Basic inspection
 # print("Shape of dataset:", df.shape)
 # print("\nColumn names:\n", df.columns.tolist())
