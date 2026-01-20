7e49ace4f8e9fafa2a9d45dedc1b5ec012303316
Harsh <agnihotriharsh154@gmail.com>
2026-01-20 08:57:50 -0800

    minor changes of data

--- src/load_data.py
+++ src/load_data.py
@@ -12,8 +12,38 @@ df = pd.read_csv("data/messy_customer_sales_data.csv")
 # print(df.shape)
 
 # print(df.columns)
+df['Gender'] = df['Gender'].str.strip().str.lower()
+df['Gender'] = df['Gender'].replace({
+    'm': 'Male',
+    'male': 'Male',
+    'f': 'Female',
+    'female': 'Female'
+})
 
-print(df.head())
+gender_purchase = df.groupby("Gender")["Purchase_Amount"].sum()
+gender_purchase2 = df.groupby("Gender")["Purchase_Amount"].value_counts()
+
+print(gender_purchase)
+print('the end')
+print(gender_purchase2)
+
+
+
+
+# print(df['Gender'])
+
+# # print(df.head())
+# print( df["Gender"].value_counts())
+
+
+
+
+# print( df[df["Country"] == "India"])
+# print(df[df["City"] == "CHENNAI"])
+
+
+# print(df[df["Purchase_Amount"].isnull()])
+
 
 # print(df[["sector", "society", "rate", "area"]])
 # # Basic inspection
@@ -25,7 +55,7 @@ print(df.head())
 
 # print("\nData info:")
 # print(df.info())
-print((df['Gender']=='M').sum())
+# print((df['Gender']=='M').sum())
 
 print("\nMissing values:")
 # print(df.isnull().sum())
