c7bc36bcfe12a6d5fc37ccc0785a218dd7fc4d2e
Harsh <agnihotriharsh154@gmail.com>
2026-01-26 05:15:47 -0800

    minor changes

--- src/load_data.py
+++ src/load_data.py
@@ -3,29 +3,39 @@ import pandas as pd
 # Load dataset
 df = pd.read_csv("data/messy_customer_sales_data.csv")
 
-# print(df.info())
-# print(df.describe())
-# print(df.head(10))
-# print(df.head())
-# print(df.tail())
+print(df.info())
+print(df.describe())
+print(df.head(10))
+print(df.head())
+print(df.tail())
 
-# print(df.shape)
+print(df.shape)
 
-# print(df.columns)
+print(df.columns)
 df['Gender'] = df['Gender'].str.strip().str.lower()
 df['Gender'] = df['Gender'].replace({
     'm': 'Male',
     'male': 'Male',
     'f': 'Female',
-    'female': 'Female'
-})
+    'female': 'Female' 
+# })
 
-gender_purchase = df.groupby("Gender")["Purchase_Amount"].sum()
-gender_purchase2 = df.groupby("Gender")["Purchase_Amount"].value_counts()
+# gender_purchase = df.groupby("Gender")["Purchase_Amount"].sum()
+# gender_purchase2 = df.groupby("Gender")["Purchase_Amount"].value_counts()
 
-print(gender_purchase)
-print('the end')
-print(gender_purchase2)
+# print(gender_purchase)
+# print('the end')
+# print(gender_purchase2)
+# print(df.info)
+
+# print(df[df["Name"=='delhi']])
+
+# print(df[df["City"] == "Delhi"])
+# print(df[df["City"].str.lower() == "delhi"])
+# print(df[df["City"].str.strip().str.lower() == "delhi"])
+
+# df[df["Customer_id"].between(1, 100)]
+print(df[pd.to_numeric(df["Customer_ID"], errors="coerce").between(1, 100)])
 
 
 
